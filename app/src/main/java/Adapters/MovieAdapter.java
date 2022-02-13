dd8554dfe5e04d929d8d2e6a3cca10e42e06fb6f
Noel1827 <noel.vargas5@upr.edu>
2022-02-13 19:41:55 -0400

    testing

--- app/src/main/java/Adapters/MovieAdapter.java
+++ app/src/main/java/Adapters/MovieAdapter.java
@@ -71,6 +71,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
                 imageUrl = movie.getPosterPath();
             }
             Glide.with(context).load(imageUrl).into(ivPoster);
+            // testing commit
         }
     }
 }
